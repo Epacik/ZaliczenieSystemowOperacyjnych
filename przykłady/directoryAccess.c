5bb1a7ae8b580599a6265bf7c99653100a84f71d
Epat <epaciatko@gmail.com>
2020-06-04 18:27:29 +0200

    przykłady dla katalogów

--- przykłady/directoryAccess.c
+++ przykłady/directoryAccess.c
@@ -28,4 +28,56 @@ void ErrLogAndClear(){
 
 int main(void) {
     setlocale(0, "pl_PL");
+
+    errno = 0;
+
+    //odczytywanie obecnego katalogu
+    char currDir[PATH_MAX];
+    getcwd(currDir, sizeof(currDir));
+    printf("Obecny katalog\n%s\n", currDir);
+
+    //tworzenie katalogu
+    int res = mkdir("./jakisKatalog", S_IRUSR | S_IWUSR | S_IXUSR);
+    printf("res: %d\n", res);
+
+    if(res == -1){
+        ErrLogAndClear();
+    }
+
+    errno = 0;
+    // Otwieranie katalogu
+    DIR* dir = opendir(".");
+
+    if(dir == NULL){
+        ErrLogAndClear();
+        return -1;
+    }
+
+    struct dirent *dp;
+
+    errno = 0;
+    //wypisywanie zawartości katalogu
+    puts("Zawartość katalogu:\n");
+    while ((dp = readdir (dir)) != NULL){
+        printf("%s\n",dp->d_name);
+    }
+
+    if(dp == NULL && errno != 0){
+        ErrLogAndClear();
+        return -1;
+    }
+    else if(dp == NULL){
+        puts("Koniec zawartości katalogu");
+    }
+
+    errno = 0;
+    // zmiana katalogu
+    res = chdir("./jakisKatalog");
+    if(res == -1){
+        ErrLogAndClear();
+    }
+
+    //sprawdzanie czy katalog rzeczywiście został zmieniony
+    getcwd(currDir, sizeof(currDir));
+    printf("Obecny katalog\n%s\n", currDir);
 }
--- przykłady/fileAccess.c
+++ przykłady/fileAccess.c
@@ -22,24 +22,27 @@ void ErrLogAndClear(){
 int main(void) {
     setlocale(0, "pl_PL");
 
+    errno = 0;
     //Otwieranie pliku do zapisu i tworzenie go jeśli nie istniał wcześniej
     int fdesc = open("./testowyPlik.txt", O_WRONLY | O_CREAT);
     if (fdesc == -1) {
-        puts("Błąd otwarcia pliku");
-        return 1;
+        ErrLogAndClear();
+        return -1;
     }
     puts("Pomyślnie otwarto plik");
 
 #pragma region Zapis do pliku
+
     char buf[] = "Jakiś tekst do zapisania w pliku\0";
     int size = strlen(buf);
 
-    int res = write(fdesc, buf, strlen(buf));
+    int res = write(fdesc, buf, size);
     if(res == -1)
     {
-        puts("Wystąpił błąd zapisu do pliku");
-        return 1;
+        ErrLogAndClear();
+        return -1;
     }
+
 #pragma endregion
     
     //zamykanie pliku
@@ -54,16 +57,27 @@ int main(void) {
     printf("%d\n", position);
 
 #pragma region Odczytywanie z pliku
+
     char* readed = malloc(sizeof(char) * size + 1 );
 
     res = read(fdesc, readed, size);
     readed[size] = '\0';
+    if(res == -1){
+        ErrLogAndClear();
+        return -1;
+    }
+
 #pragma endregion
+
     puts(readed);
 
     free(readed);
 
     //zamykanie pliku
     res = close(fdesc);
+    if(res == -1){
+        ErrLogAndClear();
+        return -1;
+    }
     return 0;
 }
